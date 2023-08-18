c85e7b8043773ec0644defe08ff2ca855dd0edce
Ose <ose.sunday@gmail.com>
2023-08-18 05:53:39 +0100

    posgres

--- greatkart_project/settings.py
+++ greatkart_project/settings.py
@@ -63,13 +63,20 @@ WSGI_APPLICATION = 'greatkart_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# Database Configuration
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NMIfVajAcEA1lMo5GQJI',
+        'HOST': 'containers-us-west-189.railway.app',
+        'PORT': '6837',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
