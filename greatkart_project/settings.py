fab06983c676eabe78c32c8c01b50897a1a18fd3
Ose <ose.sunday@gmail.com>
2023-08-19 22:05:55 +0100

    ser

--- greatkart_project/settings.py
+++ greatkart_project/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
-STATICFILES_DIRS = [STATIC_DIR, 'static',]
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # Default primary key field type
